6caaf3cd93ce9d319ea03cf8a6b807c07c6c6c84
unknown <utcungg@gmail.com>
2021-01-30 17:25:49 +0700

    first commit

---
+++ src/service/api-frontend.ts
@@ -0,0 +1,36 @@
+import { IAPI } from "./types";
+import { Todo, TodoStatus } from "../models/todo";
+import shortid from "shortid";
+
+const mockToken = "testabc.xyz.ahk";
+
+class ApiFrontend extends IAPI {
+  async signIn(username: string, password: string): Promise<string> {
+    if (username === "firstUser" && password === "example") {
+      return Promise.resolve(mockToken);
+    }
+    alert("Something Wrong!!");
+    return Promise.reject("Incorrect username/password");
+  }
+
+  async createTodo(content: string): Promise<Todo> {
+    return Promise.resolve({
+      content: content,
+      created_date: new Date().toISOString(),
+      status: TodoStatus.ACTIVE,
+      id: shortid(),
+      user_id: "firstUser",
+    } as Todo);
+  }
+
+  async getTodos(): Promise<Todo[]> {
+    const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+    if (todos) {
+      return todos;
+    }
+
+    return [];
+  }
+}
+
+export default new ApiFrontend();
---
+++ src/store/reducer.ts
@@ -0,0 +1,84 @@
+import { stringify } from "querystring";
+import { Todo, TodoStatus } from "../models/todo";
+import {
+  AppActions,
+  CREATE_TODO,
+  DELETE_ALL_TODOS,
+  DELETE_TODO,
+  TOGGLE_ALL_TODOS,
+  UPDATE_TODO_STATUS,
+  SET_TODO,
+} from "./actions";
+
+export interface AppState {
+  todos: Array<Todo>;
+}
+
+export const initialState: AppState = {
+  todos: [],
+};
+
+function reducer(state: AppState, action: AppActions): AppState {
+  switch (action.type) {
+    case CREATE_TODO:
+      state.todos.push(action.payload);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+      return {
+        ...state,
+      };
+    case SET_TODO:
+      return{
+        ...state,
+        todos: action.payload
+      }
+      
+
+    case UPDATE_TODO_STATUS:
+      const index2 = state.todos.findIndex(
+        (todo) => todo.id === action.payload.todoId
+      );
+      state.todos[index2].status = action.payload.checked
+        ? TodoStatus.COMPLETED
+        : TodoStatus.ACTIVE;
+
+      return {
+        ...state,
+        todos: state.todos,
+      };
+
+    case TOGGLE_ALL_TODOS:
+      const tempTodos = state.todos.map((e) => {
+        return {
+          ...e,
+          status: action.payload ? TodoStatus.COMPLETED : TodoStatus.ACTIVE,
+        };
+      });
+
+      return {
+        ...state,
+        todos: tempTodos,
+      };
+
+    case DELETE_TODO:
+      const index1 = state.todos.findIndex(
+        (todo) => todo.id === action.payload
+      );
+      state.todos.splice(index1, 1);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+
+      return {
+        ...state,
+        todos: state.todos,
+      };
+    case DELETE_ALL_TODOS:
+      localStorage.removeItem('todos');
+      return {
+        ...state,
+        todos: [],
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
